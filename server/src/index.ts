557775934f9c929e224c4569f7a60d471b146659
tatiankris <tatiankris@mail.ru>
2023-07-04 14:13:00 +0300

    update cors url

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import {appRouter} from "./router";
-import {createContext} from "./context";
+import express from "express";
+import cors from "cors";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./router";
+import { createContext } from "./context";
 
-
-const app = express()
-
-app.use(cors(
-    { origin: "http://localhost:3000" }
-))
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(
-    '/trpc',
-    trpcExpress.createExpressMiddleware({
-        router: appRouter,
-        createContext,
-    }),
-)
+  cors({
+    origin: ["https://buy-crypto-trpc.vercel.app/", "http://localhost:3000"],
+  })
+);
 
-app.listen(4000, () => {
-    console.log('Server running on port 4000')
-})
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
